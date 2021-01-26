c9c6b10e33d979a3a810e93b75ab3fa2ff043771
revengemiroz <mirozxy@gmail.com>
2021-01-26 20:15:37 +0545

    check if it works

--- client/src/components/Header/Header.js
+++ client/src/components/Header/Header.js
@@ -54,6 +54,11 @@ function Header(props) {
         leftIcon: <Bell />,
       },
       {
+        title: 'Edit Profile',
+        onClick: () => history.push('/admin'),
+        leftIcon: <Bell />,
+      },
+      {
         title: 'Log Out',
         onClick: () => setModalIsOpen(true),
         leftIcon: <Cog />,
