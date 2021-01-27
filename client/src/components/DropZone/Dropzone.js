820d155646bf0f9a54068850576238689f8bebf5
revengemiroz <mirozxy@gmail.com>
2021-01-27 23:42:54 +0545

    Make dropzone components

---
+++ client/src/components/DropZone/Dropzone.js
@@ -0,0 +1,26 @@
+import React from 'react';
+import { useDropzone } from 'react-dropzone';
+import { Container } from './style';
+
+export const Dropzone = ({ onDrop, accept = '' }) => {
+  //todo give warning on invalid file types
+  const {
+    getInputProps,
+    getRootProps,
+    isDragAccept,
+    isDragActive,
+    isDragReject,
+  } = useDropzone({ onDrop, maxFiles: 1, multiple: false, accept });
+
+  return (
+    <Container {...getRootProps({ isDragAccept, isDragActive, isDragReject })}>
+      <input {...getInputProps()} />
+
+      {isDragActive ? (
+        <p>Drop here</p>
+      ) : (
+        <p>Drag 'n' drop, or click to select files</p>
+      )}
+    </Container>
+  );
+};
