a9f9617723e2b7ad016f2395982e8128ecaee29f
revengemiroz <mirozxy@gmail.com>
2021-01-31 23:23:08 +0545

    Create card details component

--- client/src/components/Cardv2/CardDetails/CardDetails.js
+++ client/src/components/Cardv2/CardDetails/CardDetails.js
@@ -3,11 +3,34 @@ import { useParams } from 'react-router-dom';
 import { loader } from 'graphql.macro';
 import { useQuery } from '@apollo/client';
 
+import Button from '../../Button/Button';
 import Header from '../../Header/Header';
 
-import { Main, Container, DetailsContainer, UserDetails } from './style';
+import { ReactComponent as Github } from '../../../assets/github.svg';
+
+import {
+  Main,
+  Container,
+  ImgContainerOuter,
+  DetailsContainer,
+  UserDetails,
+  Status,
+  AllDetails,
+  ButtonContainer,
+  CustomDeleteButtonCSS,
+} from './style';
 
 const GET_PROJECT_QUERY = loader('./queryGetProject.graphql');
+
+const getCurrentDate = (createdDate) => {
+  const dateOptions = {
+    day: 'numeric',
+    month: 'short',
+    year: 'numeric',
+  };
+  const newDate = new Date(createdDate);
+  return newDate.toLocaleDateString('en-us', dateOptions);
+};
 
 export const CardDetails = ({}) => {
   const { projectId } = useParams();
@@ -36,22 +59,68 @@ export const CardDetails = ({}) => {
       <Container>
         <DetailsContainer>
           <div className='imgUserDetails'>
-            <div className='imgContainerOuter'>
+            <ImgContainerOuter status={Project.isApproved}>
               <div className='imgContainerInner'>
                 <img src={Project.preview} />
               </div>
-            </div>
+            </ImgContainerOuter>
 
             <UserDetails>
-              <span>
+              <span className='fullName'>
                 {Project.author.name} {Project.author.lastName}
               </span>
-              <span>{Project.createdAt}</span>
+              <Status status={Project.author.role}>
+                {Project.author.role}
+              </Status>
             </UserDetails>
           </div>
-          <div className='allDetails'>
-            <div></div>
-          </div>
+
+          <AllDetails>
+            <div>
+              <span className='fullName'>{Project.title}</span>
+              <span className='date'>{getCurrentDate(Project.createdAt)}</span>
+
+              <div className='linksContainer'>
+                <span>
+                  <Github /> <a href={Project.repoLink}>Github</a>
+                </span>
+                <span>
+                  <Github />
+                  <a>Contact</a>
+                </span>
+                <span>
+                  <Github />
+                  <a href={Project.siteLink}></a>
+                  Live Site
+                </span>
+              </div>
+
+              <div className='description'>{Project.description}</div>
+            </div>
+
+            <ButtonContainer>
+              <Button
+                maxWidth='big'
+                fontSize='medium'
+                kind='delete'
+                size='medium'
+                onClick={() => {}}
+                addCSS={CustomDeleteButtonCSS}
+              >
+                Delete
+              </Button>
+              <Button
+                maxWidth='small'
+                fontSize='medium'
+                kind='edit'
+                size='small'
+                onClick={() => {}}
+                addCSS={CustomDeleteButtonCSS}
+              >
+                Edit
+              </Button>
+            </ButtonContainer>
+          </AllDetails>
         </DetailsContainer>
       </Container>
     </Main>
--- client/src/components/Cardv2/CardDetails/style.js
+++ client/src/components/Cardv2/CardDetails/style.js
@@ -1,4 +1,9 @@
-import styled from 'styled-components';
+import styled, { css } from 'styled-components';
+
+const colors = {
+  green: 'rgba(0, 203, 91, 0.7)',
+  red: ' rgba(237, 44, 73, 0.7)',
+};
 
 export const Main = styled.div`
   display: flex;
@@ -13,6 +18,7 @@ export const Container = styled.div`
   align-items: center;
   width: 100%;
   background-color: #f7f8fc;
+  padding-top: 100px;
 `;
 
 export const DetailsContainer = styled.div`
@@ -23,23 +29,13 @@ export const DetailsContainer = styled.div`
   flex-direction: row;
   justify-content: space-around;
   flex-wrap: wrap;
-  border: 2px solid green;
   padding: 15px;
 
   & .imgUserDetails {
-    border: 2px solid green;
     width: 55%;
   }
 
-  & .imgContainerOuter {
-    border-radius: 5px;
-    padding: 7px;
-    border: 2px solid green;
-    background-color: #00cb5b;
-  }
-
   & .imgContainerInner {
-    border: 2px solid green;
   }
 
   img {
@@ -49,20 +45,95 @@ export const DetailsContainer = styled.div`
     object-fit: contain;
     border-radius: 5px;
   }
+`;
 
-  & .allDetails {
-    border: 2px solid green;
-    height: 400px;
-    width: 40%;
-  }
+export const ImgContainerOuter = styled.div`
+  border-radius: 5px;
+  padding: 7px;
+  background-color: ${(props) => (props.status ? colors.green : colors.red)};
 `;
 
 export const UserDetails = styled.div`
   margin-top: 10px;
-  border: 2px solid red;
 
   span {
+    display: block;
+    padding: 0;
+    margin: 0;
+    box-sizing: border-box;
+  }
+
+  & .fullName {
     text-transform: capitalize;
+    font-weight: 600;
+    font-size: 30px;
+  }
+`;
+
+export const AllDetails = styled.div`
+  display: flex;
+  flex-direction: column;
+  justify-content: space-between;
+  width: 30%;
+
+  span {
     display: block;
   }
+
+  & .fullName {
+    text-transform: capitalize;
+    font-weight: 600;
+    font-size: 35px;
+  }
+
+  & .date {
+    font-size: 20px;
+  }
+
+  & .linksContainer {
+    display: flex;
+    flex-direction: row;
+    margin-top: 15px;
+    justify-content: space-between;
+
+    span {
+      display: flex;
+      flex-direction: row;
+
+      a {
+        margin-left: 10px;
+      }
+    }
+  }
+
+  & .description {
+    margin-top: 15px;
+  }
+`;
+
+export const ButtonContainer = styled.div`
+  display: flex;
+  justify-content: space-between;
+  margin: 20px 0 0 0;
+`;
+
+export const CustomDeleteButtonCSS = css`
+  letter-spacing: 1px;
+  font-weight: 500;
+  max-width: 200px;
+`;
+
+export const Status = styled.div`
+  margin-top: 5px;
+  width: 100px;
+  font-weight: 600;
+  border-radius: 3px;
+  padding: 5px 0;
+  font-size: 14px;
+  background-color: ${(props) =>
+    props.status == 'ADMIN' ? '#D1383D' : '#00CB5B'};
+  color: white;
+  font-family: 'Poppins', sans-serif;
+  letter-spacing: 1px;
+  text-align: center;
 `;
