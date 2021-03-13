92dcd570b35f94e344698d569f8888c2a9fbaaef
Ivan Saldano <ivanms1@gmail.com>
2021-03-13 10:42:22 +0900

    lazy load routes

--- client/src/Routes/Routes.js
+++ client/src/Routes/Routes.js
@@ -1,62 +1,66 @@
-import React from 'react';
+import React, { lazy, Suspense } from 'react';
 import { Switch } from 'react-router-dom';
-
-import Home from '../pages/Home/Home';
-import Contact from '../pages/Contact/Contact';
-import Register from '../pages/Register/Register';
-import SignIn from '../pages/SignIn/Signin';
-import Logout from '../pages/Logout/Logout';
-import Submitproject from '../pages/SubmitYourProject/Submit';
-import Weekly from '../pages/WeeklyProjects/Weekly';
-import Favorites from '../pages/Favorites/Favorites';
-import Error from '../pages/Error/error';
-import Admin from '../pages/Admin/Admin';
-import Edit from '../pages/Edit/Edit';
-import CardDetails from '../components/Cardv2/CardDetails/CardDetails';
 
 import PublicRoute from './PublicRoute';
 import PrivateRoute from './PrivateRoute';
 
+const Home = lazy(() => import('../pages/Home/Home'));
+const Register = lazy(() => import('../pages/Register/Register'));
+const Contact = lazy(() => import('../pages/Contact/Contact'));
+const SignIn = lazy(() => import('../pages/SignIn/Signin'));
+const Logout = lazy(() => import('../pages/Logout/Logout'));
+const Submitproject = lazy(() => import('../pages/SubmitYourProject/Submit'));
+const Weekly = lazy(() => import('../pages/WeeklyProjects/Weekly'));
+const Favorites = lazy(() => import('../pages/Favorites/Favorites'));
+const Error = lazy(() => import('../pages/Error/error'));
+const Admin = lazy(() => import('../pages/Admin/Admin'));
+const Edit = lazy(() => import('../pages/Edit/Edit'));
+const CardDetails = lazy(() =>
+  import('../components/Cardv2/CardDetails/CardDetails')
+);
+
 function Routes() {
   return (
-    <Switch>
-      <PrivateRoute exact path='/'>
-        <Home />
-      </PrivateRoute>
-      <PrivateRoute path='/contact'>
-        <Contact />
-      </PrivateRoute>
-      <PublicRoute path='/register'>
-        <Register />
-      </PublicRoute>
-      <PublicRoute path='/signin'>
-        <SignIn />
-      </PublicRoute>
-      <PrivateRoute isForAdmin='true' path='/admin'>
-        <Admin />
-      </PrivateRoute>
-      <PrivateRoute path='/logout'>
-        <Logout />
-      </PrivateRoute>
-      <PrivateRoute path='/edit/:projectId'>
-        <Edit />
-      </PrivateRoute>
-      <PrivateRoute path='/submit'>
-        <Submitproject />
-      </PrivateRoute>
-      <PrivateRoute path='/weekly'>
-        <Weekly />
-      </PrivateRoute>
-      <PrivateRoute path='/favorites'>
-        <Favorites />
-      </PrivateRoute>
-      <PrivateRoute path='/projectDetails/:projectId'>
-        <CardDetails />
-      </PrivateRoute>
-      <PublicRoute>
-        <Error />
-      </PublicRoute>
-    </Switch>
+    <Suspense fallback={<span>Loading...</span>}>
+      <Switch>
+        <PrivateRoute exact path='/'>
+          <Home />
+        </PrivateRoute>
+        <PrivateRoute path='/contact'>
+          <Contact />
+        </PrivateRoute>
+        <PublicRoute path='/register'>
+          <Register />
+        </PublicRoute>
+        <PublicRoute path='/signin'>
+          <SignIn />
+        </PublicRoute>
+        <PrivateRoute isForAdmin='true' path='/admin'>
+          <Admin />
+        </PrivateRoute>
+        <PrivateRoute path='/logout'>
+          <Logout />
+        </PrivateRoute>
+        <PrivateRoute path='/edit/:projectId'>
+          <Edit />
+        </PrivateRoute>
+        <PrivateRoute path='/submit'>
+          <Submitproject />
+        </PrivateRoute>
+        <PrivateRoute path='/weekly'>
+          <Weekly />
+        </PrivateRoute>
+        <PrivateRoute path='/favorites'>
+          <Favorites />
+        </PrivateRoute>
+        <PrivateRoute path='/projectDetails/:projectId'>
+          <CardDetails />
+        </PrivateRoute>
+        <PublicRoute>
+          <Error />
+        </PublicRoute>
+      </Switch>
+    </Suspense>
   );
 }
 
