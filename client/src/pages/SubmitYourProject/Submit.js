3130062789d82e4c26f3f177189116b8c467042b
revengemiroz <miroz.devkota@gmail.com>
2021-03-17 12:16:58 +0545

    Use submit modal

--- client/src/pages/SubmitYourProject/Submit.js
+++ client/src/pages/SubmitYourProject/Submit.js
@@ -1,9 +1,11 @@
-import React from 'react';
+import React, { useState } from 'react';
+import { useHistory } from 'react-router-dom';
 import { useMutation, gql } from '@apollo/client';
 import { loader } from 'graphql.macro';
 
 import Header from '../../components/Header/Header';
 import ProjectForm from '../../components/Form/ProjectForm';
+import SubmissionModal from '../../components/PopupModal/SubmissionModal';
 import Loader from '../../components/Loader/Loader';
 
 import useCurrentUser from '../../components/useCurrentUser/useCurrentUser';
@@ -13,6 +15,13 @@ const CREATE_PROJECT_MUTATION = loader('./mutationCreateProject.graphql');
 
 function Submit() {
   const { currentUser: user, loading: currentUserLoading } = useCurrentUser();
+
+  const [submitValues, setSubmitValues] = useState(null);
+  const [submitModelIsOpen, setSubmitModelIsOpen] = useState(false);
+  const openSubmitModal = () => setSubmitModelIsOpen(true);
+  const closeSubmitModal = () => setSubmitModelIsOpen(false);
+
+  const history = useHistory();
 
   const [createProject] = useMutation(CREATE_PROJECT_MUTATION, {
     update(cache, { data: { createProject } }) {
@@ -54,6 +63,23 @@ function Submit() {
     return <Loader />;
   }
 
+  function submitTheProject(values) {
+    createProject({
+      variables: {
+        input: {
+          authorId: user.id,
+          preview: values.preview,
+          title: values.title,
+          siteLink: values.siteLink,
+          repoLink: values.repoLink,
+          description: values.description,
+          tags: values.tags.map((e) => e.value),
+        },
+      },
+    });
+    openSubmitModal();
+  }
+
   return (
     <Main>
       <Header />
@@ -64,24 +90,17 @@ function Submit() {
             <span>so that people can learn from each other.</span>
           </p>
           <ProjectForm
-            onSubmit={(values) =>
-              createProject({
-                variables: {
-                  input: {
-                    authorId: user.id,
-                    preview: values.preview,
-                    title: values.title,
-                    siteLink: values.siteLink,
-                    repoLink: values.repoLink,
-                    description: values.description,
-                    tags: values.tags.map((e) => e.value),
-                  },
-                },
-              })
-            }
+            onSubmit={(values) => {
+              submitTheProject(values);
+            }}
           />
         </Container>
       </Overlay>
+
+      <SubmissionModal
+        isOpen={submitModelIsOpen}
+        onRequestClose={closeSubmitModal}
+      />
     </Main>
   );
 }
