d85b1381da5120bac5cc632850b1cd5a66947195
revengemiroz <mirozxy@gmail.com>
2021-03-28 16:28:07 +0545

    make new mobile-menu

--- client/src/components/Layout/index.js
+++ client/src/components/Layout/index.js
@@ -1,13 +1,26 @@
+import React, { useState } from 'react';
 import Routes from '../../Routes';
 import Header from '../Header';
+
+import BurgerIcon from '../BurgerIcon';
+import MobileSideMenu from '../MobileSideMenu';
 
 import { Main } from './style';
 
 function Layout() {
+  const [openMobileMenu, setOpenMobileMenu] = useState(false);
   return (
     <Main>
       <Header />
       <Routes />
+      <BurgerIcon
+        onClick={() => setOpenMobileMenu(true)}
+        open={openMobileMenu}
+      />
+      <MobileSideMenu
+        isOpen={openMobileMenu}
+        onRequestClose={() => setOpenMobileMenu(false)}
+      />
     </Main>
   );
 }
---
+++ client/src/components/MobileSideMenu/style.js
@@ -0,0 +1,54 @@
+import styled from 'styled-components';
+import Modal from 'react-modal';
+import { Link } from 'react-router-dom';
+
+export const StyledModal = styled(Modal)`
+  position: absolute;
+  bottom: 25px;
+  right: 25px;
+  box-shadow: 10px 10px 40px 4px rgba(45, 78, 255, 0.2) !important;
+  border-radius: 4px;
+  background-color: white;
+  padding: 12px;
+  width: 250px;
+  height: 160px;
+  z-index: 999 !important;
+  display: grid;
+  grid-gap: 5px;
+  grid-template-rows: repeat(2, 1fr);
+  grid-template-columns: repeat(3, 1fr);
+  /* border: 2px solid green; */
+`;
+
+export const Menu = styled(Link)`
+  display: flex;
+  flex-direction: column;
+  justify-content: center;
+  align-items: center;
+  text-decoration: none;
+  color: black;
+
+  :hover {
+    transform: scale(1.02) !important;
+    transition: 0.1s linear !important;
+  }
+
+  :active {
+    transform: translateY(1px) scale(1.01) !important;
+  }
+  /* border: 2px solid green; */
+
+  span {
+    text-align: center;
+    font-size: 12px;
+    color: #647380;
+    margin-top: 7px;
+  }
+`;
+
+export const Exit = styled.button`
+  background-color: transparent;
+  outline: none;
+  border: none;
+  /* border: 2px solid green; */
+`;
